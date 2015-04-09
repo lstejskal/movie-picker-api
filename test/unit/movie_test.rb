a0028617faf663461fceda91562fb216362df78d
Lukas Stejskal <lucastej@gmail.com>
2015-04-09 21:27:26 +0200

    [TESTS] Added tests for Movie model

--- test/unit/movie_test.rb
+++ test/unit/movie_test.rb
@@ -23,6 +23,25 @@ module MoviePicker
           assert_equal 5, movies.size
         end
 
+        should "add movie" do
+          Movie.add('Jurrasic Park')
+          movies = Movie.all
+          assert_equal 6, movies.size
+          assert movies.include?('Jurrasic Park')
+        end
+
+        should "pick random movie from list" do
+          movie = Movie.all.first
+
+          Movie.redis.expects(:srandmember).returns(movie)
+
+          Movie.pick_random
+
+          movies = Movie.all
+          assert_equal 4, movies.size
+          assert ! movies.include?(movie)
+        end
+
       end
 
     end
